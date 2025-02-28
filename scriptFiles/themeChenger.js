540e840ce0d8b8170f4ca1c94644e9edac54b140
SKMPranto <prantokumar3463@gmail.com>
2025-02-28 23:11:25 +0600

    Nav section added

---
+++ scriptFiles/themeChenger.js
@@ -0,0 +1,11 @@
+
+const btn = document.getElementById("btn-themeChanger");
+let randomNum = () => {
+    return Math.floor(Math.random() * 256);
+};
+let changeColor = () => {
+    let color = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    document.body.style.backgroundColor = color;
+};
+btn.addEventListener("click", changeColor);
+
