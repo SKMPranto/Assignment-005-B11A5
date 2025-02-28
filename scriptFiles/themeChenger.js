3643dd839768333a3476da96e6a7a8c122b87225
SKMPranto <prantokumar3463@gmail.com>
2025-03-01 00:33:30 +0600

    Task bar section added

--- scriptFiles/themeChenger.js
+++ scriptFiles/themeChenger.js
@@ -1,11 +1,9 @@
-
 const btn = document.getElementById("btn-themeChanger");
 let randomNum = () => {
-    return Math.floor(Math.random() * 256);
+  return Math.floor(Math.random() * 256);
 };
 let changeColor = () => {
-    let color = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
-    document.body.style.backgroundColor = color;
+  let color = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+  document.body.style.backgroundColor = color;
 };
 btn.addEventListener("click", changeColor);
-
