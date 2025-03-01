62a422e36223530e6fd0a6840f14ed3282042f64
SKMPranto <prantokumar3463@gmail.com>
2025-03-01 13:04:12 +0600

    realtime for chalange part added

--- scriptFiles/utilities.js
+++ scriptFiles/utilities.js
@@ -1,6 +1,41 @@
-function jumpPage(id,page){
-    document.getElementById(id).addEventListener("click", function (){
-        const url = window.location.href=page;
+function jumpPage(id, page) {
+  document.getElementById(id).addEventListener("click", function () {
+    const url = (window.location.href = page);
     return url;
-});
-};
+  });
+}
+
+
+
+
+// Function to update the date and day on the page
+
+function updateDate() {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const realTime = new Date();
+
+  const day = days[realTime.getDay()];
+  const date = realTime.getDate();
+  const month = months[realTime.getMonth()];
+  const year = realTime.getFullYear();
+
+  document.getElementById("currentWeekday").textContent = `${day} ,`;
+  document.getElementById(
+    "currentDate"
+  ).textContent = `${date} ${month} ${year}`;
+}
+window.onload = updateDate;
