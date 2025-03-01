fb7efa5c5b0add0cbd395d4a3a5b387045c1e414
SKMPranto <prantokumar3463@gmail.com>
2025-03-01 21:23:26 +0600

    Project is ready

--- scriptFiles/main.js
+++ scriptFiles/main.js
@@ -10,5 +10,28 @@ for (let btnClicked of btnCompleted) {
     calculateTotalTasks(assignedValue, "taskAssigned");
     const completedTask = getInnerTextValueById("completedTaskNum");
     calculateCompletedTasks(completedTask, "completedTaskNum");
+    // history log
+    let card = btnClicked.parentElement.parentElement;
+    let cardTitle = card.querySelector(".cardTitle").innerText;
+    // Realtime
+    let realTime = new Date();
+    let hours = realTime.getHours();
+    let minutes = realTime.getMinutes();
+    let amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    let currentTime = `${hours}:${minutes < 10 ? "0" : ""}${minutes} ${amPm}`;
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="w-[318px] bg-slate-100 rounded-xl p-2 my-3">
+    <p>You have Complete The Task <span class="font-bold"> ${cardTitle} </span> <span>${currentTime}</span></p>
+    </div>
+    `;
+    document.getElementById("historyContainer").append(div);
+    // Clear the history
+    document
+      .getElementById("btnClearHistory")
+      .addEventListener("click", function () {
+        document.getElementById("historyContainer").innerHTML = "";
+      });
   });
 }
